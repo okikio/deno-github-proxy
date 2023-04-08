8458985a710ca799aa112913204ac21accbfe505
Okiki Ojo <okikio.dev@gmail.com>
2023-04-08 17:22:14 -0400

    ...

--- complex-service.ts
+++ complex-service.ts
@@ -1,5 +1,4 @@
-import db from 'http://localhost:8000/neo4j/neo4j-javascript-driver/5.0/packages/neo4j-driver-deno/lib/mod.ts';
-import type * as neo4j from 'http://localhost:8000/neo4j/neo4j-javascript-driver/5.0/packages/neo4j-driver-deno/lib/mod.ts';
+import * as neo4j from 'https://github-ts.deno.dev/neo4j/neo4j-javascript-driver/5.0/packages/neo4j-driver-deno/lib/mod.ts';
 
 /**
  * Create a new driver instance to connect to Neo4j
@@ -7,10 +6,10 @@ import type * as neo4j from 'http://localhost:8000/neo4j/neo4j-javascript-driver
  * @see https://neo4j.com/docs/api/javascript-driver/current/class/src/driver.js~Driver.html
  * @see https://neo4j.com/docs/api/javascript-driver/current/global.html#Config
  */
-const driver: neo4j.Driver = db.driver(
+const driver: neo4j.Driver = neo4j.driver(
                     // ^ Does type inferencing work?
   "",
-  db.auth.basic( "", "" )
+  neo4j.auth.basic( "", "" )
 );
 
 export default driver;
