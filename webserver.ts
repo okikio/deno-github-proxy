0add2c40e70ec84015996c986366794b4c3accae
Okiki Ojo <okikio.dev@gmail.com>
2023-04-08 17:02:06 -0400

    ...

--- webserver.ts
+++ webserver.ts
@@ -3,17 +3,17 @@ import { serve } from "https://deno.land/std@0.182.0/http/server.ts";
 const DENO_TYPE_PATH = "/@deno-types"
 
 serve(async (req: Request) => {
-  let { pathname } = new URL(req.url);
+  let { pathname, searchParams } = new URL(req.url);
 
   const url = new URL(pathname.replace(DENO_TYPE_PATH, ""), `https://raw.githubusercontent.com`);
   const res = await fetch(url);
 
   const headers = Object.fromEntries([...res.headers]);
-  const typescriptTypes = new URL(DENO_TYPE_PATH + url.pathname, "https://8000-gitpodsampl-templatetyp-gxqkxjx6j7m.ws-us93.gitpod.io");
+  const typescriptTypes = new URL(DENO_TYPE_PATH + url.pathname, "https://brave-parrot-49.deno.dev");
   const finalHeaders = {
     ...headers,
     ...Object.fromEntries([
-      ["content-type", "text/typescript"], // pathname.startsWith(DENO_TYPE_PATH) ? "application/typescript" : 
+      ["content-type", searchParams.has("js") ? "text/javascript" : "text/typescript"],
       ['accept-ranges', 'bytes'],
       ['access-control-allow-origin', '*'],
       ['cache-control', 'max-age=30, public'],
