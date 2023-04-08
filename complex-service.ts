86403e7e8a756e755f192137f90a7e7f38804f84
Okiki Ojo <okikio.dev@gmail.com>
2023-04-08 17:26:45 -0400

    ...

--- complex-service.ts
+++ complex-service.ts
@@ -7,9 +7,11 @@ import * as neo4j from 'https://github-ts.deno.dev/neo4j/neo4j-javascript-driver
  * @see https://neo4j.com/docs/api/javascript-driver/current/global.html#Config
  */
 const driver: neo4j.Driver = neo4j.driver(
-                    // ^ Does type inferencing work?
-  "",
-  neo4j.auth.basic( "", "" )
+  Deno.env.get('NEO4J_URI') as string,
+  neo4j.auth.basic( 
+    Deno.env.get('NEO4J_USERNAME') as string, 
+    Deno.env.get('NEO4J_PASSWORD') as string 
+  )
 );
 
 export default driver;
